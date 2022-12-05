984fb3d5b0ed1a6ec42dc206f07ba1e05c3de1ed
Семён Капустников <s.v.kapustnikov@gmail.com>
2022-12-05 14:38:00 +0300

    test31

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -1,19 +1,6 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-# class IsOwnerOrAdminOrReadOnly(BasePermission):
-#     def has_permission(self, request, view):
-#         if request.method == 'POST':
-#             return request.user.is_authenticated
-#         return request.method in SAFE_METHODS
-
-#     def has_object_permission(self, request, view, obj):
-#         if request.META['HTTP_REFERER'][-5:] == 'edit/' and request.user != obj.author:
-#             return False
-#         elif request.method in SAFE_METHODS or request.user.is_superuser:
-#             return True
-#         else:
-#             return True
 class IsOwnerOrAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method == 'POST':
@@ -21,6 +8,21 @@ class IsOwnerOrAdminOrReadOnly(BasePermission):
         return True
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS or request.user.is_superuser:
+        if request.META['HTTP_REFERER'][-5:] == 'edit/' and request.user != obj.author:
+            return False
+        elif request.method in SAFE_METHODS or request.user.is_superuser:
             return True
-        return request.user == obj.author
+        else:
+            return True
+
+
+# class IsOwnerOrAdminOrReadOnly(BasePermission):
+#     def has_permission(self, request, view):
+#         if request.method == 'POST':
+#             return request.user.is_authenticated
+#         return True
+
+#     def has_object_permission(self, request, view, obj):
+#         if request.method in SAFE_METHODS or request.user.is_superuser:
+#             return True
+#         return request.user == obj.author
