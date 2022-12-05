d558d99e58ca511ee72cce409fb97fbe6a39a692
Семён Капустников <s.v.kapustnikov@gmail.com>
2022-12-05 21:38:18 +0300

    front for user

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -9,7 +9,7 @@ class IsOwnerOrAdminOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         if request.user != obj.author and (
-                request.META['HTTP_REFERER'][-5:] in ['edit/', '/edit']):
+                request.META['HTTP_REFERER'][-5:] in ('edit/', '/edit')):
             return False
         elif request.method in SAFE_METHODS or request.user.is_superuser:
             return True
