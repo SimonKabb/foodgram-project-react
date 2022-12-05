76a1869744e0565e8e5e58141e72cf97b9350969
Семён Капустников <s.v.kapustnikov@gmail.com>
2022-12-05 13:53:05 +0300

    test20

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -13,4 +13,5 @@ class IsOwnerOrAdminOrReadOnly(BasePermission):
         if ((request.method in SAFE_METHODS or request.user.is_superuser)
                 and request.META['HTTP_REFERER'][-5:] != 'edit/'):
             return True
-        return request.user == obj.author
+        else:
+            return request.user == obj.author
