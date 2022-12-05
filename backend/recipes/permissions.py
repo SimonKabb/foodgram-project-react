fe965b253321746ee541c091c391410a58781ddb
Семён Капустников <s.v.kapustnikov@gmail.com>
2022-12-05 14:34:31 +0300

    test30

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -1,16 +1,26 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
+# class IsOwnerOrAdminOrReadOnly(BasePermission):
+#     def has_permission(self, request, view):
+#         if request.method == 'POST':
+#             return request.user.is_authenticated
+#         return request.method in SAFE_METHODS
+
+#     def has_object_permission(self, request, view, obj):
+#         if request.META['HTTP_REFERER'][-5:] == 'edit/' and request.user != obj.author:
+#             return False
+#         elif request.method in SAFE_METHODS or request.user.is_superuser:
+#             return True
+#         else:
+#             return True
 class IsOwnerOrAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method == 'POST':
             return request.user.is_authenticated
-        return request.method in SAFE_METHODS
+        return True
 
     def has_object_permission(self, request, view, obj):
-        if request.META['HTTP_REFERER'][-5:] == 'edit/' and request.user != obj.author:
-            return False
-        elif request.method in SAFE_METHODS or request.user.is_superuser:
+        if request.method in SAFE_METHODS or request.user.is_superuser:
             return True
-        else:
-            return True
+        return request.user == obj.author
